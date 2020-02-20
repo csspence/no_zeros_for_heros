5f0682112c5686c2e2a50bdea1114b6035829fc8
Crew Spence <csspence123@gmail.com>
2020-02-20 06:42:51 -0600

    Wrote working solution for no_zeros_for_heros.

--- no_zeros_for_heros.js
+++ no_zeros_for_heros.js
@@ -12,6 +12,24 @@ Get rid of them. Only the ending ones.
 Zero alone is fine, don't worry about it. Poor guy anyway
 */
 
-function noBoringZeros(n) {
-  // your code
+const noBoringZeros = (n) => {
+  let answer = n.toString();
+  let lastNum = answer.length - 1;
+  if(n === 0) {
+    return 0;
+  }
+
+  const innerFunc = () => {
+    answer = answer.slice(0, lastNum);
+    lastNum = answer.length - 1;
+    if(answer[lastNum] === '0') {
+      innerFunc();
+    }
+  }
+
+  if(answer[lastNum] === '0') {
+    innerFunc();
+  }
+
+  return parseInt(answer);
 }
